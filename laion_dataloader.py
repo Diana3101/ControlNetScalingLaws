c956a6467936c3409649bfe0c0da5905adeec210
Diana3101 <diana15kapatsyn@gmail.com>
2024-03-20 19:13:34 +0000

    update hf dataloader, filter urls for people

--- laion_dataloader.py
+++ laion_dataloader.py
@@ -6,11 +6,13 @@ from PIL import Image
 from torch.utils.data import DataLoader
 import torch
 from torchvision import transforms
-
+from tqdm import tqdm
 
 data_dir = '/shared_drive/user-files/laion_dataset_200M/laion200m-data'
+device = torch.device('cuda')
 
-def make_train_dataset(data_dir, seed, buffer_size, resolution):
+
+def make_train_dataset(data_dir, n_loader, num_loaders):
     def get_folders_in_parent_directory(parent_dir):
         folders = []
         for entry in os.listdir(parent_dir):
@@ -27,37 +29,69 @@ def make_train_dataset(data_dir, seed, buffer_size, resolution):
         tar_subset_files = [os.path.join(subset_folder, file) for file in tar_subset_files if file.endswith('.tar')] 
         webdataset_files.extend(tar_subset_files)
 
-    iterable_dataset = load_dataset("webdataset", data_files={"train": webdataset_files}, split="train", 
+    portion_count = int(len(webdataset_files)/ num_loaders)
+    webdataset_files_loader_subset = webdataset_files[portion_count*n_loader : portion_count*(n_loader+1)]
+
+    webdataset_files_loader_subset_valid = []
+    for file in webdataset_files_loader_subset:
+        file_json = file.replace('.tar', '_stats.json')
+        if os.path.exists(file_json):
+            webdataset_files_loader_subset_valid.append(file)
+
+    # webdataset_files_loader_subset_valid_FOLDERS = [file.split('/')[-2] for file in webdataset_files_loader_subset_valid]
+    # webdataset_files_loader_subset_valid_FOLDERS = list(set(webdataset_files_loader_subset_valid_FOLDERS))
+    # print(f'Loader {n_loader+1} contain images from folders: ')
+    # print(webdataset_files_loader_subset_valid_FOLDERS)
+    # print()
+
+    iterable_dataset = load_dataset("webdataset", data_files={"train": webdataset_files_loader_subset_valid}, split="train", 
                        streaming=True)
-    shuffled_iterable_dataset = iterable_dataset.shuffle(seed=seed, buffer_size=buffer_size)
-
-    # train_dataset = shuffled_iterable_dataset.select_columns(['jpg', 'txt'])
-
-    # def get_transformed_image(example):
-    #     image = example['jpg']
-    #     image = image.convert("RGB")
-
-    #     image_transforms = transforms.Compose(
-    #         [
-    #             transforms.Resize(resolution, interpolation=transforms.InterpolationMode.BILINEAR),
-    #             transforms.CenterCrop(resolution),
-    #             transforms.ToTensor(),
-    #             transforms.Normalize([0.5], [0.5]),
-    #         ]
-    #     )
-
-    #     example['pixel_values'] = image_transforms(image)
-    #     return example
-
-    def get_url(example):
-        url = example['json']['url']
-        example['url'] = url
-        return example
     
-    # train_dataset = train_dataset.map(get_transformed_image)
-    shuffled_iterable_dataset = shuffled_iterable_dataset.map(get_url)
+    # shuffled_iterable_dataset = iterable_dataset.shuffle(seed=seed, buffer_size=buffer_size)
+    # import pdb; pdb.set_trace()
 
-    return shuffled_iterable_dataset
+    def get_detr_transformed_image_and_urls(example):
+        new_example = {}
+        url = example['json']['url']
+
+        image = example['jpg']
+        image = image.convert("RGB")
+
+        # for detr model
+        image_mean = [
+            0.485,
+            0.456,
+            0.406
+        ]
+
+        image_std = [
+            0.229,
+            0.224,
+            0.225
+        ]
+
+        image_transforms = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.ConvertImageDtype(torch.float16),
+                transforms.Normalize(image_mean, image_std),
+            ]
+        )
+
+        new_example['pixel_values'] = image_transforms(image)
+        new_example['url'] = url
+        
+        return new_example
+
+    # def get_url(example):
+    #     url = example['json']['url']
+    #     example['url'] = url
+    #     return example
+    
+    iterable_dataset = iterable_dataset.map(get_detr_transformed_image_and_urls)
+    # iterable_dataset = iterable_dataset.map(get_url)
+
+    return iterable_dataset
 
 
 # train_dataset = make_train_dataset(data_dir=data_dir,
@@ -88,10 +122,28 @@ def make_train_dataset(data_dir, seed, buffer_size, resolution):
 #     for example in my_iterable_dataset:  # fast + reshuffled at each epoch using `effective_seed = seed + epoch`
 #         pass
 
-# dataloader = DataLoader(train_dataset, batch_size=32, 
-#                         num_workers=4,
+
+# def collate_fn(train_dataset):
+#     pixel_values = torch.stack([example["pixel_values"] for example in train_dataset])
+#     urls = [example["url"] for example in train_dataset]
+#     return pixel_values, urls
+
+
+# IMAGE_RESOLUTION = 256
+# train_dataset = make_train_dataset(data_dir=data_dir, n_loader=0, num_loaders=6)
+# train_dataset = make_train_dataset(data_dir=data_dir, n_loader=1, num_loaders=6)
+# train_dataset = make_train_dataset(data_dir=data_dir, n_loader=2, num_loaders=6)
+# train_dataset = make_train_dataset(data_dir=data_dir, n_loader=3, num_loaders=6)
+# train_dataset = make_train_dataset(data_dir=data_dir, n_loader=4, num_loaders=6)
+# train_dataset = make_train_dataset(data_dir=data_dir, n_loader=5, num_loaders=6)
+
+# dataloader = DataLoader(train_dataset, batch_size=128, 
+#                         num_workers=1,
 #                         collate_fn=collate_fn)
 
-# for step, batch in enumerate(dataloader):
+# for step, batch in tqdm(enumerate(dataloader)):
+#     batch_tensor = batch[0]
+    # batch_tensor = batch_tensor.to(device)
+#     batch_urls = batch[1]
 #     print(step)
 #     import pdb; pdb.set_trace() 
--- object_detector_hf_dataloader.py
+++ object_detector_hf_dataloader.py
@@ -8,38 +8,35 @@ from tqdm import tqdm
 import numpy as np
 import pandas as pd
 import pdb
+from transformers.image_transforms import center_to_corners_format
+from typing import List, Union
+import torch.nn as nn
+from time import time
+import pdb
+import argparse
+
+# import multiprocessing
+# multiprocessing.set_start_method('spawn')
+# print(multiprocessing.get_start_method())
+
+
+import wandb
+from wandb import AlertLevel
+# wandb.login()
 
 from laion_dataloader import make_train_dataset
 
-data_dir = '/shared_drive/user-files/laion_dataset_200M/laion200m-data'
+DATA_DIR = '/shared_drive/user-files/laion_dataset_200M/laion200m-data'
 IMAGE_RESOLUTION = 256
-device = torch.device('cuda')
-batch_size = 128
-num_workers = 6
+DEVICE = torch.device('cuda')
+BATCH_SIZE = 128
+NUM_WORKERS = 2
 
-train_dataset = make_train_dataset(data_dir=data_dir,
-                                   seed=42, buffer_size=100, resolution=IMAGE_RESOLUTION)
-
-# you can specify the revision tag if you don't want the timm dependency
-processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
 model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
-
-model = model.to(device)
-
-target_sizes = torch.tensor([[IMAGE_RESOLUTION, IMAGE_RESOLUTION]]*batch_size).to(device)
-
-def collate_fn(train_dataset): 
-    images = []
-    urls = []
-    
-    for example in train_dataset:
-        images.append(example['jpg'].convert("RGB"))
-        urls.append(example['url'])
-        
-    return processor(images=images, return_tensors="pt", do_resize=False), urls
+model = model.to(DEVICE, dtype=torch.float16)
 
 
-def save_results_to_parquet(results, urls, model, step):
+def save_results_to_parquet(results, urls, model, step, n_loader):
     rows = []
     columns = ['url', 'label', 'score', 'top_left_x', 'top_left_y', 'bottom_right_x', 'bottom_right_y']
     for i, result_per_image in enumerate(results):
@@ -54,29 +51,136 @@ def save_results_to_parquet(results, urls, model, step):
             rows.append(row)
         
     df = pd.DataFrame(rows, columns=columns)
-    df.to_parquet(f'/shared_drive/user-files/laion_dataset_200M/od-test/{step}_batch.parquet')
+    df.to_parquet(f'/mnt/disks/disk-big2/laion200m-od-labels-loader{n_loader+1}/{step}_batch.parquet')
+
+def post_process_object_detection(
+        outputs, threshold: float = 0.5, target_sizes = None
+    ):
+        out_logits, out_bbox = outputs.logits, outputs.pred_boxes
+
+        if target_sizes is not None:
+            if len(out_logits) != len(target_sizes):
+                raise ValueError(
+                    "Make sure that you pass in as many target sizes as the batch dimension of the logits"
+                )
+
+        # t = time()
+        prob = nn.functional.softmax(out_logits, -1)
+        scores, labels = prob[..., :-1].max(-1)
+        # print(time() - t)
+
+        # t = time()
+        # Convert to [x0, y0, x1, y1] format
+        boxes = center_to_corners_format(out_bbox)
+        # print(time() - t)
+
+        mask = scores > threshold
+    
+        # mask = mask.cpu()
+        # scores = scores.cpu()
+        # labels = labels.cpu()
+        # boxes = boxes.cpu()
+
+        # t = time()
+        # Convert from relative [0, 1] to absolute [0, height] coordinates
+        if target_sizes is not None:
+            if isinstance(target_sizes, List):
+                img_h = torch.Tensor([i[0] for i in target_sizes])
+                img_w = torch.Tensor([i[1] for i in target_sizes])
+            else:
+                img_h, img_w = target_sizes.unbind(1)
+
+            scale_fct = torch.stack([img_w, img_h, img_w, img_h], dim=1).to(boxes.device)
+            boxes = boxes * scale_fct[:, None, :]
+        # print(time() - t)
+
+        # t = time()
+        results = []
+    
+        # print(len(scores))
+        # mask_idx, class_idx = torch.where(mask)
+        # class_idx[i, i*128]
+
+        for i in range(len(scores)):
+        # for s, l, b in zip(scores, labels, boxes):
+            score = scores[i][mask[i]]
+            label = labels[i][mask[i]]
+            box = boxes[i][mask[i]]
+            # print(s.shape)
+            results.append({"scores": score, "labels": label, "boxes": box})
+        # print(time() - t)
+        # print()
+
+        return results
 
 
-dataloader = DataLoader(train_dataset, 
-                        batch_size=batch_size,
-                        num_workers=num_workers,
+def collate_fn(train_dataset):
+    pixel_values = torch.stack([example["pixel_values"] for example in train_dataset])
+    urls = [example["url"] for example in train_dataset]
+    return pixel_values, urls
+
+
+def run_od_model(args):
+    train_dataset = make_train_dataset(data_dir=DATA_DIR, n_loader=args.n_loader, num_loaders=args.num_loaders)
+
+    dataloader = DataLoader(train_dataset, 
+                        batch_size=BATCH_SIZE,
+                        num_workers=NUM_WORKERS,
                         pin_memory=True,
                         collate_fn=collate_fn)
+    
+    t = time()
+    for step, batch in tqdm(enumerate(dataloader)):
+        # pdb.set_trace()
+        try:
+            batch_tensor = batch[0]
+            batch_urls = batch[1]
+            batch_tensor = batch_tensor.to(DEVICE)
 
+            batch_dict = {'pixel_values': batch_tensor}
+            
+            outputs = model(**batch_dict)
+            
+            target_sizes = torch.tensor([[IMAGE_RESOLUTION, IMAGE_RESOLUTION]]*batch_tensor.shape[0]).to(DEVICE)
+            results = post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)
+            
+            save_results_to_parquet(results=results, 
+                                                urls=batch_urls, 
+                                                model=model, 
+                                                step=step,
+                                                n_loader=args.n_loader)
 
-for step, batch in tqdm(enumerate(dataloader)):
-    batch_urls = batch[1]
-    batch_tensors = batch[0]
+            wandb.log({f"loader-{args.n_loader}_batch": step})
 
-    batch_tensors['pixel_values'] = batch_tensors['pixel_values'].to(device)
-    batch_tensors['pixel_mask'] = batch_tensors['pixel_mask'].to(device)
+        except:
+            wandb.alert(title=f"Batch Warning!",
+                        text=f"Problem with batch {step}",
+                        level=AlertLevel.WARN)
+            pdb.set_trace()
 
-    outputs = model(**batch_tensors)
+    print(f"Time for {args.n_loader}: {time() - t} sec")
+    wandb.alert(title=f"Run {args.n_loader} finished!",
+            text = f"Objects successfully detected in all {step+1} batches in {np.round((time() - t)/3600, 2)} hours !!! :)",
+                   level=AlertLevel.INFO)
+   
 
-    results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--n_loader", type=int)
+    parser.add_argument("--num_loaders", type=int, default=6)
+    args = parser.parse_args()
+    return args
 
-    save_results_to_parquet(results=results, 
-                                 urls=batch_urls, 
-                                 model=model, 
-                                 step=step)
-
+if __name__ == "__main__":
+    args = parse_args()
+    
+    run = wandb.init(
+            # Set the project where this run will be logged
+            project="object-detector",
+            # Track hyperparameters and run metadata
+            config={
+            },
+        )
+    run_od_model(args)
+    
+    wandb.finish()
