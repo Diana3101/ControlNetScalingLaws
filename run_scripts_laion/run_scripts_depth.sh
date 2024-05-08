3c0f8cc2b5f4165bd5dbb8ca9ec9edba6a50d15d
Diana3101 <user@inst-builder-debian-12-build-build-1myyn.c.gce-image-builder.internal>
2024-05-08 08:47:48 +0000

    fix max_train_steps, add README

--- run_scripts_laion/run_scripts_depth.sh
+++ run_scripts_laion/run_scripts_depth.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 accelerate launch train_laion_controlnet_webdataset.py \
  --pretrained_model_name_or_path="runwayml/stable-diffusion-v1-5" \
- --dataset_name="scaling-laws-diff-exp/laion1k" \
- --dataset_length=1000 \
+ --dataset_name="scaling-laws-diff-exp/laion500k" \
+ --dataset_length=500000 \
  --download_dataset_before_streaming \
- --cache_dir="/shared_drive/user-files/huggingface/datasets/cache" \
- --output_dir="/mnt/disks/disk_1tb/checkpoints_laion/laion1k-depth-16ksteps" \
- --dataloader_num_workers=1 \
+ --cache_dir="/mnt/disk_60tb/huggingface/cache/datasets/" \
+ --output_dir="/mnt/disk_60tb/checkpoints_laion/laion500k-depth" \
+ --dataloader_num_workers=12 \
  --mixed_precision="bf16" \
  --resolution=512 \
  --condition_type='depth' \
  --validation_set_folder='validation_set_laion_depth' \
- --validation_steps=500 \
+ --validation_steps=1000 \
  --num_validation_images=1 \
- --train_batch_size=32 \
- --gradient_accumulation_steps=16 \
+ --train_batch_size=8 \
+ --gradient_accumulation_steps=8 \
  --gradient_checkpointing \
  --allow_tf32 \
  --use_8bit_adam \
  --learning_rate=1e-4 \
  --lr_scheduler="constant_with_warmup" \
  --lr_warmup_steps=4000 \
- --checkpointing_steps=500 \
+ --checkpointing_steps=1000 \
  --seed=0 \
- --num_train_epochs=8000 \
+ --num_train_epochs=16 \
  --report_to=wandb \
  --tracker_project_name='laion_train_controlnet_MAIN'
+#   --resume_from_checkpoint="/mnt/disk_60tb/checkpoints_laion/laion500k-depth/checkpoint-1000" \
--- train_laion_controlnet_webdataset.py
+++ train_laion_controlnet_webdataset.py
@@ -1039,7 +1039,7 @@ def main(args):
     
     # Scheduler and math around the number of training steps.
     overrode_max_train_steps = False
-    num_update_steps_per_epoch = math.ceil((args.dataset_length/args.train_batch_size) / args.gradient_accumulation_steps)
+    num_update_steps_per_epoch = math.ceil(((args.dataset_length/args.train_batch_size) / args.gradient_accumulation_steps) / accelerator.num_processes)
     if args.max_train_steps is None:
         args.max_train_steps = args.num_train_epochs * num_update_steps_per_epoch
         overrode_max_train_steps = True
@@ -1082,7 +1082,7 @@ def main(args):
     text_encoder.to(accelerator.device, dtype=weight_dtype)
 
     # We need to recalculate our total training steps as the size of the training dataloader may have changed.
-    num_update_steps_per_epoch = math.ceil((args.dataset_length/args.train_batch_size) / args.gradient_accumulation_steps)
+    num_update_steps_per_epoch = math.ceil(((args.dataset_length/args.train_batch_size) / args.gradient_accumulation_steps) / accelerator.num_processes)
     if overrode_max_train_steps:
         args.max_train_steps = args.num_train_epochs * num_update_steps_per_epoch
     # Afterwards we recalculate our number of training epochs
@@ -1104,7 +1104,8 @@ def main(args):
 
     logger.info("***** Running training *****")
     logger.info(f"  Num examples = {(args.dataset_length/args.train_batch_size)*args.train_batch_size}")
-    logger.info(f"  Num batches each epoch = {args.dataset_length/args.train_batch_size}")
+    # logger.info(f"  Num batches each epoch = {args.dataset_length/args.train_batch_size}")
+    logger.info(f"  Num batches each epoch = {args.dataset_length/total_batch_size}")
     logger.info(f"  Num Epochs = {args.num_train_epochs}")
     logger.info(f"  Instantaneous batch size per device = {args.train_batch_size}")
     logger.info(f"  Total train batch size (w. parallel, distributed & accumulation) = {total_batch_size}")
